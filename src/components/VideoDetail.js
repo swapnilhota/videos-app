2d67107ef017bde145baf87dbbb0738e2dea0325
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-16 11:38:22 +0530

    load cricket videos by default

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -11,7 +11,7 @@ const VideoDetail = ({ video }) => {
     return (
         <div>
             <div className="ui embed">
-                <iframe src={videoSrc} />
+                <iframe src={videoSrc} title="video player" />
             </div>
             <div className="ui segment">
                 <h4 className="ui header">
