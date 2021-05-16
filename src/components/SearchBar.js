d3a2a2bcd4f469c396533eef3a4cca95b040516f
Swapnil Hota <swapnilhota99@gmail.com>
2021-05-16 10:01:53 +0530

    added onChange listener in search input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = { term: '' };
+
+    onInputChange = () => {
+
+    };
+
     render() {
         return (
-            <div>Search Bar</div>
+            <div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
